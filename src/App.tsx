93a7f6c5e7be3282db745036e54f2f8351151256
ame-f <ameflok@udel.edu>
2026-02-15 14:15:14 -0500

    html/css finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,40 @@
 import React from "react";
 import "./App.css";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import hamster from "./assets/images/hamter.jpg";
 
 function App(): React.JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
+                <p>Amelie Flokiewicz</p>
             </header>
-            <p>Amelie Flokiewicz</p>
-            <p>
-                Hello World!
-                <img src="../assets/hamter.jpg" alt="hamster." />
-            </p>
+            <Container>
+                <Row>
+                    <Col>
+                        <h1>Hello World!</h1> <br></br>
+                        <Button
+                            onClick={() => {
+                                console.log("Hello World!");
+                            }}
+                        >
+                            Log Hello World
+                        </Button>
+                        <br></br>
+                        <ul>
+                            <li>Bacon</li>
+                            <li>Lettuce</li>
+                            <li>Tomato</li>
+                        </ul>
+                        <div id="rectangle"></div>
+                    </Col>
+                    <Col>
+                        <img src={hamster} alt="hamster." />
+                        <div id="rectangle"></div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
