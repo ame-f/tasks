87fecae6b1e5467a5b4bbf8e94f3e35909a378b3
ame-f <ameflok@udel.edu>
2026-03-08 22:15:54 -0400

    finished task 9

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App(): React.JSX.Element {
                 <p>Amelie Flokiewicz</p>
             </header>
             <hr></hr>
-            {/* <DoubleHalf></DoubleHalf> */}
+            <DoubleHalf></DoubleHalf>
             <hr></hr>
             <ChooseTeam></ChooseTeam>
             <hr></hr>
--- src/bad-components/ColoredBox.tsx
+++ src/bad-components/ColoredBox.tsx
@@ -2,45 +2,33 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export const COLORS = ["red", "blue", "green"];
-const DEFAULT_COLOR_INDEX = 0;
-
-function ChangeColor(): React.JSX.Element {
-    const [colorIndex, setColorIndex] = useState<number>(DEFAULT_COLOR_INDEX);
-    return (
-        <Button
-            onClick={() => {
-                setColorIndex((1 + colorIndex) % COLORS.length);
-            }}
-        >
-            Next Color
-        </Button>
-    );
-}
-
-function ColorPreview(): React.JSX.Element {
-    return (
-        <div
-            data-testid="colored-box"
-            style={{
-                width: "50px",
-                height: "50px",
-                backgroundColor: COLORS[DEFAULT_COLOR_INDEX],
-                display: "inline-block",
-                verticalAlign: "bottom",
-                marginLeft: "5px",
-            }}
-        ></div>
-    );
-}
 
 export function ColoredBox(): React.JSX.Element {
+    const [colorIndex, setColorIndex] = useState<number>(0);
+
     return (
         <div>
             <h3>Colored Box</h3>
-            <span>The current color is: {COLORS[DEFAULT_COLOR_INDEX]}</span>
+            <span>The current color is: {COLORS[colorIndex]}</span>
+            <Button
+                onClick={() => {
+                    setColorIndex((colorIndex + 1) % COLORS.length);
+                }}
+            >
+                Next Color
+            </Button>
             <div>
-                <ChangeColor></ChangeColor>
-                <ColorPreview></ColorPreview>
+                <div
+                    data-testid="colored-box"
+                    style={{
+                        width: "50px",
+                        height: "50px",
+                        backgroundColor: COLORS[colorIndex],
+                        display: "inline-block",
+                        verticalAlign: "bottom",
+                        marginLeft: "5px",
+                    }}
+                ></div>
             </div>
         </div>
     );
